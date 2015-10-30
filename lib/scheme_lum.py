ec463f681b8d1106ec7b5d8d016a0ee66e7a81d6
Isaac <isaacmuse@gmail.com>
2015-10-30 10:32:00 -0600

    Strip comments from plist before parsing

--- lib/scheme_lum.py
+++ lib/scheme_lum.py
@@ -1,3 +1,8 @@
+"""
+Scheme Luminance.
+
+Determines the scheme luminance to discover if the theme is light or dark.
+"""
 import sublime
 from plistlib import readPlistFromBytes
 from .rgba import RGBA
@@ -6,6 +11,8 @@ import re
 
 
 def sublime_format_path(pth):
+    """Format the path for sublime."""
+
     m = re.match(r"^([A-Za-z]{1}):(?:/|\\)(.*)", pth)
     if sublime.platform() == "windows" and m is not None:
         pth = m.group(1) + "/" + m.group(2)
@@ -13,11 +20,13 @@ def sublime_format_path(pth):
 
 
 def scheme_lums(scheme_file):
+    """Get the scheme lumincance."""
     color_scheme = os.path.normpath(scheme_file)
     scheme_file = os.path.basename(color_scheme)
     plist_file = readPlistFromBytes(
-        sublime.load_binary_resource(
-            sublime_format_path(color_scheme)
+        re.sub(
+            br"^[\r\n\s]*<!--[\s\S]*?-->[\s\r\n]*|<!--[\s\S]*?-->", b'',
+            sublime.load_binary_resource(sublime_format_path(color_scheme))
         )
     )
 
