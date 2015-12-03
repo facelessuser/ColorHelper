4b991ad4a7df3bcb5456d70d55b827b09dccaa07
facelessuser <faceless.shop@gmail.com>
2015-12-02 23:47:13 -0700

    Add rebeccapurple

--- lib/csscolors.py
+++ lib/csscolors.py
@@ -125,6 +125,7 @@ name2hex_map = {
     'plum': '#dda0dd',
     'powderblue': '#b0e0e6',
     'purple': '#800080',
+    'rebeccapurple': '#663399',
     'red': '#ff0000',
     'rosybrown': '#bc8f8f',
     'royalblue': '#4169e1',
