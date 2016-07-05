a8a8fc596c0dfd3c796dba4d238a5731e750a4cd
facelessuser <faceless.shop@gmail.com>
2016-07-05 00:46:18 -0600

    Add code to support mdpopups 1.7.0

--- color_helper_util.py
+++ color_helper_util.py
@@ -4,6 +4,9 @@ import re
 import decimal
 from ColorHelper.lib import csscolors
 from ColorHelper.lib.rgba import RGBA, round_int, clamp
+from textwrap import dedent
+import mdpopups
+RELATIVE_FONT_SUPPORT = mdpopups.version() >= (1, 7, 0)
 
 FLOAT_TRIM_RE = re.compile(r'^(?P<keep>\d+)(?P<trash>\.0+|(?P<keep2>\.\d*[1-9])0+)$')
 
@@ -73,11 +76,22 @@ COLOR_RE = re.compile(r'(?x)(?i)(?!<[@#$.\-_])(?:%s|%s)(?![@#$.\-_])' % (COMPLET
 COLOR_ALL_RE = re.compile(r'(?x)(?i)(?!<[@#$.\-_])(?:%s|%s|%s)(?![@#$.\-_])' % (COMPLETE, COLOR_NAMES, INCOMPLETE))
 INDEX_ALL_RE = re.compile((r'(?x)(?i)(?!<[@#$.\-_])(?:%s|%s)(?![@#$.\-_])' % (COMPLETE, COLOR_NAMES)).encode('utf-8'))
 
-ADD_CSS = '''
-.color-helper.content { margin: 0; padding: 0.5em; }
-.color-helper.small { font-size: 0.8em; }
-.color-helper.alpha { text-decoration: underline; }
-'''
+if RELATIVE_FONT_SUPPORT:
+    ADD_CSS = dedent(
+        '''
+        .color-helper.content { margin: 0; padding: 0.5em; }
+        .color-helper.small { font-size: {{'*.7'|relativesize('em')}}; }
+        .color-helper.alpha { text-decoration: underline; }
+        '''
+    )
+else:
+    ADD_CSS = dedent(
+        '''
+        .color-helper.content { margin: 0; padding: 0.5em; }
+        .color-helper.small { font-size: 0.8em; }
+        .color-helper.alpha { text-decoration: underline; }
+        '''
+    )
 
 CSS3 = ("webcolors", "hex", "hex_compressed", "rgb", "rgba", "hsl", "hsla")
 CSS4 = CSS3 + ("gray", "graya", "hwb", "hwba", "hexa", "hexa_compressed")
@@ -111,7 +125,7 @@ def color_picker_available():
 
 
 def fmt_float(f, p=0):
-    """Set float pring precision and trim precision zeros."""
+    """Set float precision and trim precision zeros."""
 
     string = str(
         decimal.Decimal(f).quantize(decimal.Decimal('0.' + ('0' * p) if p > 0 else '0'), decimal.ROUND_HALF_UP)
