941029c3dd2d08f902a65c92ae0813c36d0f1f8d
facelessuser <faceless.shop@gmail.com>
2016-11-27 08:27:41 -0700

    Support improvements
    - Consolidate changes.py and support.py
    - Add support to render docs for things like quick start guide.
    - Add command to open external links.

--- support.py
+++ support.py
@@ -2,9 +2,21 @@
 import sublime
 import sublime_plugin
 import textwrap
+import webbrowser
+import re
 
 __version__ = "2.2.0"
 __pc_name__ = 'ColorHelper'
+
+CSS = '''
+div.color-helper { padding: 10px; margin: 0; }
+.color-helper h1, .color-helper h2, .color-helper h3,
+.color-helper h4, .color-helper h5, .color-helper h6 {
+    {{'.string'|css}}
+}
+.color-helper blockquote { {{'.comment'|css}} }
+.color-helper a { text-decoration: none; }
+'''
 
 
 def list2string(obj):
@@ -90,3 +102,96 @@ class ColorHelperSupportInfoCommand(sublime_plugin.ApplicationCommand):
 
         sublime.message_dialog(msg + '\nInfo has been copied to the clipboard.')
         sublime.set_clipboard(msg)
+
+
+class ColorHelperOpenSiteCommand(sublime_plugin.ApplicationCommand):
+    """Open site links."""
+
+    def run(self, url):
+        """Open the url."""
+
+        webbrowser.open_new_tab(url)
+
+
+class ColorHelperDocCommand(sublime_plugin.WindowCommand):
+    """Open doc page."""
+
+    re_pkgs = re.compile(r'^Packages')
+
+    def on_navigate(self, href):
+        """Handle links."""
+
+        if href.startswith('sub://Packages'):
+            sublime.run_command('open_file', {"file": self.re_pkgs.sub('${packages}', href[6:])})
+        else:
+            webbrowser.open_new_tab(href)
+
+    def run(self, page):
+        """Open page."""
+
+        try:
+            import mdpopups
+            has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3118)
+        except Exception:
+            has_phantom_support = False
+
+        if not has_phantom_support:
+            sublime.run_command('open_file', {"file": page})
+        else:
+            text = sublime.load_resource(page.replace('${packages}', 'Packages'))
+            view = self.window.new_file()
+            view.set_name('ColorHelper - Quick Start')
+            view.settings().set('gutter', False)
+            view.settings().set('word_wrap', False)
+            if has_phantom_support:
+                mdpopups.add_phantom(
+                    view,
+                    'quickstart',
+                    sublime.Region(0),
+                    text,
+                    sublime.LAYOUT_INLINE,
+                    css=CSS,
+                    wrapper_class="color-helper",
+                    on_navigate=self.on_navigate
+                )
+            else:
+                view.run_command('insert', {"characters": text})
+            view.set_read_only(True)
+            view.set_scratch(True)
+
+
+class ColorHelperChangesCommand(sublime_plugin.WindowCommand):
+    """Changelog command."""
+
+    def run(self):
+        """Show the changelog in a new view."""
+        try:
+            import mdpopups
+            has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3118)
+        except Exception:
+            has_phantom_support = False
+
+        text = sublime.load_resource('Packages/ColorHelper/CHANGES.md')
+        view = self.window.new_file()
+        view.set_name('ColorHelper - Changelog')
+        view.settings().set('gutter', False)
+        view.settings().set('word_wrap', False)
+        if has_phantom_support:
+            mdpopups.add_phantom(
+                view,
+                'changelog',
+                sublime.Region(0),
+                text,
+                sublime.LAYOUT_INLINE,
+                wrapper_class="color-helper",
+                css=CSS,
+                on_navigate=self.on_navigate
+            )
+        else:
+            view.run_command('insert', {"characters": text})
+        view.set_read_only(True)
+        view.set_scratch(True)
+
+    def on_navigate(self, href):
+        """Open links."""
+        webbrowser.open_new_tab(href)
