6d5cb6c4fc6357724c8ec61bff533c20c2e9e1d9
Isaac Muse <faceless.shop@gmail.com>
2019-08-23 07:44:18 -0600

    Add minimum height cap when dealing with image sizes and allow better scaling (#135)
    * Add minimum height cap when dealing with image sizes
    
    This will help prevent errors
    
    * Tweak color picker scaling and add graphic_scale option
    
    * Tweak size logic and add fine image scaling option
    
    * Fix lint

--- color_helper_picker.py
+++ color_helper_picker.py
@@ -35,6 +35,7 @@ color_map_size = False
 color_map_style = None
 line_height = None
 default_border = None
+color_scale = None
 
 SPACER = '#00000000'
 
@@ -52,15 +53,18 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
         global color_map_style
         global line_height
         global default_border
+        global color_scale
 
         if (
             color_map is None or
             self.graphic_size != color_map_size or
+            self.graphic_scale != color_scale or
             self.line_height != line_height or
             self.default_border != default_border or
             color_map_style != "square"
         ):
             color_map_size = self.graphic_size
+            color_scale = self.graphic_scale
             color_map_style = "square"
             line_height = self.line_height
             default_border = self.default_border
@@ -167,15 +171,18 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
         global color_map_style
         global line_height
         global default_border
+        global color_scale
 
         if (
             color_map is None or
             self.graphic_size != color_map_size or
+            self.graphic_scale != color_scale or
             self.line_height != line_height or
             self.default_border != default_border or
             color_map_style != "hex"
         ):
             color_map_size = self.graphic_size
+            color_scale = self.graphic_scale
             line_height = self.line_height
             default_border = self.default_border
             color_map_style = "hex"
@@ -276,7 +283,7 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
                 '[%s](%s) %s<br>' % (
                     mdpopups.color_box(
                         [color], self.default_border,
-                        border_size=BORDER_SIZE, height=self.box_height, width=self.box_height * 8,
+                        border_size=BORDER_SIZE, height=self.height, width=self.box_height * 8,
                         check_size=check_size
                     ),
                     color,
@@ -333,7 +340,7 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
                 '[%s](%s) %s<br>' % (
                     mdpopups.color_box(
                         [color], self.default_border,
-                        border_size=BORDER_SIZE, height=self.box_height, width=self.box_height * 8,
+                        border_size=BORDER_SIZE, height=self.height, width=self.box_height * 8,
                         check_size=check_size
                     ),
                     color,
@@ -483,6 +490,9 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
 
         settings = sublime.load_settings('color_helper.sublime-settings')
         self.graphic_size = qualify_settings(settings, 'graphic_size', 'medium')
+        self.graphic_scale = qualify_settings(settings, 'graphic_scale', None)
+        if not isinstance(self.graphic_scale, (int, float)):
+            self.graphic_scale = None
         self.line_height = util.get_line_height(self.view)
         top_pad = self.view.settings().get('line_padding_top', 0)
         bottom_pad = self.view.settings().get('line_padding_bottom', 0)
@@ -491,11 +501,18 @@ class ColorHelperPickerCommand(sublime_plugin.TextCommand):
             top_pad = 0
         if bottom_pad is None:
             bottom_pad = 0
-        self.box_height = self.line_height - int(top_pad + bottom_pad) - 6
+        box_height = self.line_height - int(top_pad + bottom_pad) - 6
+        if self.graphic_scale is not None:
+            box_height = box_height * self.graphic_scale
+            self.graphic_size = "small"
+        small = max(box_height, 8)
+        medium = max(box_height * 1.5, 8)
+        large = max(box_height * 2, 8)
+        self.box_height = int(small)
         sizes = {
-            "small": (int(self.box_height * .85), int(self.box_height * .85), int(self.box_height * 1.0)),
-            "medium": (int(self.box_height), int(self.box_height), int(self.box_height * 1.25)),
-            "large": (int(self.box_height * 1.15), int(self.box_height * 1.15), int(self.box_height * 1.35))
+            "small": (int(small), int(small), int(small + small / 4)),
+            "medium": (int(medium), int(medium), int(medium + medium / 4)),
+            "large": (int(large), int(large), int(large + large / 4))
         }
         self.height, self.width, self.height_big = sizes.get(
             self.graphic_size,
