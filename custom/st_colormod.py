54a0bac2b811bbd9304c5a0d9848286b702d05f1
facelessuser <faceless.shop@gmail.com>
2021-03-03 07:53:57 -0700

    No need for _parse to be classmethod
    match future coloraide version

--- custom/st_colormod.py
+++ custom/st_colormod.py
@@ -507,23 +507,22 @@ class Color(ColorCSS):
 
         super().__init__(color, data, alpha, filters=None, variables=variables, **kwargs)
 
-    @classmethod
-    def _parse(cls, color, data=None, alpha=util.DEF_ALPHA, filters=None, variables=None, **kwargs):
+    def _parse(self, color, data=None, alpha=util.DEF_ALPHA, filters=None, variables=None, **kwargs):
         """Parse the color."""
 
         obj = None
         if data is not None:
             filters = set(filters) if filters is not None else set()
-            for space, space_class in cls.CS_MAP.items():
+            for space, space_class in self.CS_MAP.items():
                 s = color.lower()
                 if space == s and (not filters or s in filters):
                     obj = space_class(data[:space_class.NUM_COLOR_CHANNELS] + [alpha])
                     return obj
         elif isinstance(color, ColorCSS):
             if not filters or color.space() in filters:
-                obj = cls.CS_MAP[color.space()](color._color)
+                obj = self.CS_MAP[color.space()](color._color)
         else:
-            m = cls._match(color, fullmatch=True, filters=filters, variables=variables)
+            m = self._match(color, fullmatch=True, filters=filters, variables=variables)
             if m is None:
                 raise ValueError("'{}' is not a valid color".format(color))
             obj = m.color
