e1c918daea0a0cd90409494890ddc137b17fd7fb
Isaac Muse <faceless.shop@gmail.com>
2021-03-16 10:51:18 -0600

    Fix Windows color picker (#173)

--- os_color_picker.py
+++ os_color_picker.py
@@ -81,6 +81,40 @@ class WinPick(_ColorPicker):
     https://docs.microsoft.com/en-us/windows/win32/api/commdlg/ns-commdlg-choosecolorw-r1
     """
 
+    def get_win_pick_colors(self):
+        """Get windows custom colors."""
+
+        CustomColors = ctypes.c_uint32 * 16  # noqa: N806
+        colors = sublime.load_settings('color_helper.palettes').get("win_picker_custom", [])
+        length = len(colors)
+        if length > 16:
+            colors = colors[0:16]
+        if length < 16:
+            delta = 16 - length
+            colors.extend(['color(srgb 1 1 1)'] * delta)
+        for index, color in enumerate(colors, 0):
+            try:
+                hx = Color(color).to_string(hex=True, alpha=False)[1:]
+                colors[index] = int(hx[4:6] + hx[2:4] + hx[0:2], 16)
+            except Exception:
+                colors[index] = 0xffffff
+        return CustomColors(*colors)
+
+    def set_win_pick_colors(self, colors):
+        """Set windows custom colors."""
+
+        pcolors = []
+        for index in range(16):
+            hx = '{:06x}'.format(colors[index])
+            pcolors.append(
+                Color('rgb({} {} {})'.format(int(hx[4:6], 16), int(hx[2:4], 16), int(hx[0:2], 16))).to_string(
+                    color=True, fit=False, precision=-1
+                )
+            )
+        s = sublime.load_settings('color_helper.palettes')
+        s.set("win_picker_custom", pcolors)
+        sublime.save_settings('color_helper.palettes')
+
     def pick(self):
         """Pick the color."""
 
@@ -90,17 +124,17 @@ class WinPick(_ColorPicker):
 
         picker = CHOOSECOLOR()
         picker.lStructSize = ctypes.sizeof(picker)
-        CustomColors = ctypes.c_uint32 * 16  # noqa: N806
-        picker.lpCustColors = CustomColors()
+        picker.lpCustColors = self.get_win_pick_colors()
         picker.Flags = CC_SOLIDCOLOR | CC_FULLOPEN | CC_RGBINIT
         picker.rgbResult = ctypes.c_uint32(bgr)
 
         if ChooseColorW(ctypes.byref(picker)):
             hx = '{:06x}'.format(picker.rgbResult)
-            color = Color('srgb', [int(hx[4:6], 16), int(hx[2:4], 16), int(hx[0:2], 16)])
+            color = Color('rgb({} {} {})'.format(int(hx[4:6], 16), int(hx[2:4], 16), int(hx[0:2], 16)))
             self.color = color
         else:
             color = self.color
+        self.set_win_pick_colors(picker.lpCustColors)
         return color
 
 
